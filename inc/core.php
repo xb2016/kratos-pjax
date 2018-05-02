60a8b4e6754026b86f3f6b04e35db145702c100e
小白-白 <xb@fczbl.vip>
2018-05-02 13:23:15 +0800

    Update: Disable automatic formatting

--- inc/core.php
+++ inc/core.php
@@ -33,23 +33,8 @@ function kratos_active_menu_class($classes){
 }
 add_filter('nav_menu_css_class','kratos_active_menu_class');
 //Disable automatic formatting
-function my_formatter($content){
-    $new_content = '';
-    $pattern_full = '{(\[raw\].*?\[/raw\])}is';
-    $pattern_contents = '{\[raw\](.*?)\[/raw\]}is';
-    $pieces = preg_split($pattern_full,$content,-1,PREG_SPLIT_DELIM_CAPTURE);
-foreach ($pieces as $piece){
-    if(preg_match($pattern_contents,$piece,$matches)){
-        $new_content .= $matches[1];
-    }else{
-        $new_content .= wptexturize(wpautop($piece));
-    }
-}
-    return $new_content;
-}
 remove_filter('the_content','wpautop');
 remove_filter('the_content','wptexturize');
-add_filter('the_content','my_formatter',99);
 //Support chinese tags
 add_action('parse_request','kratos_chinese_tag_names_parse_request');
 add_filter('get_pagenum_link','kratos_chinese_tag_names_get_pagenum_link');
