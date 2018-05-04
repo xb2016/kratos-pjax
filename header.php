959835c598820e7251ec61dde942b830cd312216
围墙 <kms10.cn@gmail.com>
2018-05-04 21:15:10 +0800

    import BaiduPost js

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
         if(kratos_option('background_mode')=='image'&&!wp_is_mobile()&&!kratos_option('site_bw')) echo '@media(min-width:768px){.pagination>li>a{background-color:rgba(255,255,255,.8)}.kratos-hentry,.navigation div,.comments-area .comment-list li,#kratos-widget-area .widget,.comment-respond{background-color:rgba(253,253,253,.85)!important}.comment-list .children li{background-color:rgba(255,253,232,.7)!important}body.custom-background{background-image:url('.kratos_option('background_index_image').');background-size:cover;background-attachment:fixed}}';
         if(kratos_option('head_mode')=='pic') echo '@media(max-width:768px){#kratos-header-section{background:rgba('.hex2rgb(kratos_option('banner_color')).','.kratos_option('banner_color_op').')}}@media(min-width:768px){.color-logo{display:none}.affix{top:54px}}'; ?>
     </style>
+    <script type="text/javascript" src="wp-content/themes/kratos-pjax/js/BaiduPost.js"></script>
   </head>
     <?php flush(); ?>
     <body <?php if(kratos_option('background_mode')=='image') echo 'class="custom-background"'; ?>>
