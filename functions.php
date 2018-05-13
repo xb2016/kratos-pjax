4ee0e27afad8ebb3562a558dd268c32c855f8993
小白-白 <xb@fczbl.vip>
2018-05-13 10:37:39 +0800

    Update to 0.0.7

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('KRATOS_VERSION','0.0.6');
+define('KRATOS_VERSION','0.0.7');
 
 require_once(get_template_directory().'/inc/core.php');
 require_once(get_template_directory().'/inc/shortcode.php');
