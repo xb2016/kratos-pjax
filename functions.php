a28e5ad8d6160adc20b5ba7c6f7513a9bf558d93
小白-白 <xb@fczbl.vip>
2018-05-17 18:24:08 +0800

    这个版本不太清真

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('KRATOS_VERSION','0.0.8');
+define('KRATOS_VERSION','0.0.9');
 
 require_once(get_template_directory().'/inc/core.php');
 require_once(get_template_directory().'/inc/shortcode.php');
