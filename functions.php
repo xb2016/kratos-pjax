895aaf308274a9530d3152a3602ca105792add36
Tony <Tony-1998@live.cn>
2018-11-09 19:54:53 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -11,3 +11,17 @@ require_once(get_template_directory().'/inc/widgets.php');
 require_once(get_template_directory().'/inc/smtp.php');
 require_once(get_template_directory().'/inc/logincfg.php');
 require_once(get_template_directory().'/inc/avatars.php');
+
+function add_prism() {
+	wp_register_script(
+		'prismjs',
+		get_stylesheet_directory_uri() . '/static/js/prism.min.js'   
+	);
+	wp_register_style(
+		'prismcss', 
+		get_stylesheet_directory_uri() . '/static/css/prism.min.css' 
+	);
+	wp_enqueue_script('prismjs');
+	wp_enqueue_style('prismcss');
+}
+add_action('wp_enqueue_scripts', 'add_prism');
