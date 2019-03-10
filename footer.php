a915550f6ea9074e902130b373edf1c230efd88c
xb2016 <loli@html.moe>
2019-03-10 14:56:25 +0800

    [Update] New Version 0.3.9

--- footer.php
+++ footer.php
@@ -1,15 +1,5 @@
                 <footer>
                     <div id="footer"<?php echo ' style="background:rgba('.kratos_option('footer_color').')"'; ?>>
-                        <div class="cd-tool text-center">
-                            <div class="<?php if(kratos_option('cd_weixin')) echo 'gotop-box2 '; ?>gotop-box"><div class="gotop-btn"><span class="fa fa-chevron-up"></span></div></div>
-                            <?php if(kratos_option('cd_weixin')) echo '<div id="wechat-img" class="wechat-img"><span class="fa fa-weixin"></span><div id="wechat-pic"><img src="'.kratos_option('weixin_image').'"></div></div>'; ?>
-                            <div class="search-box">
-                                <span class="fa fa-search"></span>
-                                <form class="search-form" role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-                                    <input type="text" name="s" id="search" placeholder="Search..." style="display:none"/>
-                                </form>
-                            </div>
-                        </div>
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-6 col-md-offset-3 footer-list text-center">
@@ -29,113 +19,47 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="cd-tool text-center">
+                            <div class="<?php if(kratos_option('cd_weixin')) echo 'gotop-box2 '; ?>gotop-box"><div class="gotop-btn"><span class="fa fa-chevron-up"></span></div></div>
+                            <?php if(kratos_option('cd_weixin')) echo '<div id="wechat-img" class="wechat-img"><span class="fa fa-weixin"></span><div id="wechat-pic"><img src="'.kratos_option('weixin_image').'"></div></div>'; ?>
+                            <div class="search-box">
+                                <span class="fa fa-search"></span>
+                                <form class="search-form" role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
+                                    <input type="text" name="s" id="search" placeholder="Search..." style="display:none"/>
+                                </form>
+                            </div>
+                        </div>
+                        <?php if(kratos_option('site_girl')&&!wp_is_mobile()){ ?>
+                        <div class="waifu">
+                            <div class="waifu-tips"></div>
+                            <canvas id="live2d" width="220" height="250" class="live2d"></canvas>
+                            <div class="waifu-tool">
+                                <span class="fa fa-home"></span>
+                                <span class="fa fa-comments"></span>
+                                <span class="fa fa-drivers-license-o"></span>
+                                <span class="fa fa-street-view"></span>
+                                <span class="fa fa-camera"></span>
+                                <span class="fa fa-info-circle"></span>
+                                <span class="fa fa-close"></span>
+                            </div>
+                        </div>
+                        <?php }if(kratos_option('ap_footer')){ ?>
+                        <div class="aplayer-footer">
+                            <div id="ap-footer" data-json="<?php echo kratos_option('ap_json'); ?>" data-autoplay="<?php echo kratos_option('ap_autoplay'); ?>" data-loop="<?php echo kratos_option('ap_loop'); ?>" data-order="<?php echo kratos_option('ap_order'); ?>"></div>
+                        </div>
+                        <?php }if(kratos_option('site_snow')&&(!wp_is_mobile()||wp_is_mobile()&&kratos_option('snow_xb2016_mobile'))){ ?>
+                        <div class="xb-snow">
+                            <canvas id="Snow" data-count="<?php echo kratos_option('snow_xb2016_flakecount'); ?>" data-dist="<?php echo kratos_option('snow_xb2016_mindist'); ?>" data-color="<?php echo kratos_option('snow_xb2016_snowcolor'); ?>" data-size="<?php echo kratos_option('snow_xb2016_size'); ?>" data-speed="<?php echo kratos_option('snow_xb2016_speed'); ?>" data-opacity="<?php echo kratos_option('snow_xb2016_opacity'); ?>" data-step="<?php echo kratos_option('snow_xb2016_stepsize'); ?>"></canvas>
+                        </div>
+                        <?php } ?>
                     </div>
                 </footer>
             </div>
         </div>
-        <?php if(kratos_option('site_girl')&&!wp_is_mobile()){ ?>
-        <div class="waifu">
-            <div class="waifu-tips"></div>
-            <canvas id="live2d" width="220" height="250" class="live2d"></canvas>
-            <div class="waifu-tool">
-                <span class="fa fa-home"></span>
-                <span class="fa fa-comments"></span>
-                <span class="fa fa-drivers-license-o"></span>
-                <span class="fa fa-street-view"></span>
-                <span class="fa fa-camera"></span>
-                <span class="fa fa-info-circle"></span>
-                <span class="fa fa-close"></span>
-            </div>
-        </div><?php }
-        wp_footer();
-        if(kratos_option('script_tongji')||kratos_option('add_script')){ ?>
-            <script type="text/javascript">
+        <?php wp_footer();if(kratos_option('script_tongji')||kratos_option('add_script')){ ?>
+        <script type="text/javascript">
             <?php echo kratos_option('script_tongji');echo kratos_option('add_script'); ?>
-            </script><?php
-        }
-        if(kratos_option('ap_footer')){ ?>
-        <div class="aplayer-footer">
-            <div class="ap-f" id="ap-f"></div>
-            <script>
-            $(function(){
-                $.ajax({
-                    url:"<?php echo kratos_option('ap_json'); ?>",
-                    success:function(e){
-                        var a = new APlayer({
-                            element:document.getElementById("ap-f"),
-                            autoplay:<?php if(kratos_option('ap_autoplay')) echo 'true'; else echo 'false'; ?>,
-                            fixed:true,
-                            loop:"<?php echo kratos_option('ap_loop'); ?>",
-                            order:"<?php echo kratos_option('ap_order'); ?>",
-                            listFolded:true,
-                            showlrc:3,
-                            theme:"#e6d0b2",
-                            listmaxheight:"200px",
-                            music:eval(e)
-                        });
-                        window.aplayers || (window.aplayers = []),
-                        window.aplayers.push(a)
-                    }
-                })
-            })
-            </script>
-        </div><?php }
-        if(kratos_option('site_snow')&&(!wp_is_mobile()||wp_is_mobile()&&kratos_option('snow_xb2016_mobile'))){ ?>
-        <div id="xb_snow">
-            <canvas id="Snow"></canvas>
-            <script>
-            (function(){var requestAnimationFrame=window.requestAnimationFrame||window.mozRequestAnimationFrame||window.webkitRequestAnimationFrame||window.msRequestAnimationFrame||function(callback){window.setTimeout(callback,1000/60);};window.requestAnimationFrame=requestAnimationFrame;})();
-            (function(){
-                var flakes=[],canvas=document.getElementById("Snow"),ctx=canvas.getContext("2d"),flakeCount=<?php echo kratos_option('snow_xb2016_flakecount'); ?>,mX=-100,mY=-100;
-                canvas.width=window.innerWidth;
-                canvas.height=window.innerHeight;
-                function snow(){
-                    ctx.clearRect(0,0,canvas.width,canvas.height);
-                    for(var i=0;i<flakeCount;i++){
-                        var flake=flakes[i],x=mX,y=mY,minDist=<?php echo kratos_option('snow_xb2016_mindist'); ?>,x2=flake.x,y2=flake.y;
-                        var dist=Math.sqrt((x2-x)*(x2-x)+(y2-y)*(y2-y)),dx=x2-x,dy=y2-y;
-                        if(dist<minDist){
-                            var force=minDist/(dist*dist),xcomp=(x-x2)/dist,ycomp=(y-y2)/dist,deltaV=force/2;
-                            flake.velX-=deltaV*xcomp;
-                            flake.velY-=deltaV*ycomp;
-                        }else{
-                            flake.velX*=0.98;
-                            if(flake.velY<=flake.speed){flake.velY = flake.speed;}
-                            flake.velX+=Math.cos(flake.step+=.05)*flake.stepSize;
-                        }
-                        ctx.fillStyle="rgba(<?php echo kratos_option('snow_xb2016_snowcolor'); ?>,"+flake.opacity+")";
-                        flake.y+=flake.velY;
-                        flake.x+=flake.velX;
-                        if(flake.y>=canvas.height||flake.y<=0){reset(flake);}
-                        if(flake.x>=canvas.width||flake.x<=0){reset(flake);}
-                        ctx.beginPath();
-                        ctx.arc(flake.x,flake.y,flake.size,0,Math.PI*2);
-                        ctx.fill();
-                    }
-                    requestAnimationFrame(snow);
-                };
-                function reset(flake){
-                    flake.x=Math.floor(Math.random()*canvas.width);
-                    flake.y=0;
-                    flake.size=(Math.random()*3)+2;
-                    flake.speed=(Math.random()*1)+0.5;
-                    flake.velY=flake.speed;
-                    flake.velX=0;
-                    flake.opacity=(Math.random()*0.5)+0.3;
-                }
-                function init(){
-                    for(var i=0;i<flakeCount;i++){
-                        var x=Math.floor(Math.random()*canvas.width),y=Math.floor(Math.random()*canvas.height),size=(Math.random()*3)+<?php echo kratos_option('snow_xb2016_size'); ?>,speed=(Math.random()*1)+<?php echo kratos_option('snow_xb2016_speed'); ?>,opacity=(Math.random()*0.5)+<?php echo kratos_option('snow_xb2016_opacity'); ?>;
-                        flakes.push({speed:speed,velY:speed,velX:0,x:x,y:y,size:size,stepSize:(Math.random())/30*<?php echo kratos_option('snow_xb2016_stepsize'); ?>,step:0,angle:180,opacity:opacity});
-                    }
-                    snow();
-                };
-                document.addEventListener("mousemove",function(e){mX=e.clientX,mY=e.clientY});
-                window.addEventListener("resize",function(){canvas.width=window.innerWidth;canvas.height=window.innerHeight;});
-                init();
-            })();
-            </script>
-        </div>
+        </script>
         <?php } ?>
     </body>
 </html>
