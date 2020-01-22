b496ebc34c67fc36ac3f559a9437d275703baada
小白-白 <xb@fczbl.vip>
2020-01-22 11:57:56 +0800

    [Update] BUG Fix

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('KRATOS_VERSION','0.4.2');
+define('KRATOS_VERSION','0.4.3');
 
 require_once(get_template_directory().'/inc/core.php');
 require_once(get_template_directory().'/inc/shortcode.php');
